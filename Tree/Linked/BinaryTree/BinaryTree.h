8e4e22280781e4472243b0b5f646c0ea4fe81ff8
Juston007 <704549445@qq.com>
2021-05-01 11:51:51 +0800

    update binary tree
    未完成层级遍历

--- Tree/Linked/BinaryTree/BinaryTree.h
+++ Tree/Linked/BinaryTree/BinaryTree.h
@@ -24,16 +24,42 @@
 
 /*=====================================结构体=====================================*/
 typedef struct LinkedTreeNodeStruct{
-    ElementType data;                          //数据域 存储着结点的值
-    struct LinkedTreeNodeStruct *leftChild;
-    struct LinkedTreeNodeStruct *rightChild;       //指针域 指向左子结点和右子结点
+    ElementType data;                           //数据域 存储着结点的值
+    struct LinkedTreeNodeStruct *leftChild;     //指针域 指向左子结点
+    struct LinkedTreeNodeStruct *rightChild;    //指针域 指向右子结点
 } BinaryNode, *BinaryTree;
 /*=====================================结构体=====================================*/
 
 
 /*=====================================函数声明=====================================*/
+Status initBinaryTree(BinaryTree &tree);
+Status destoryBinaryTree(BinaryTree &tree);
+Status createBinaryTree(BinaryTree &tree);
+Status clearBinaryTree(BinaryTree &tree);
 Status isEmptyBinaryTree(BinaryTree &tree);
+int getBinaryTreeDepth(BinaryTree &tree);
+Status getRoot(BinaryTree &tree, ElementType &returnRoot);
+Status getValue(BinaryTree &tree, BinaryNode node, ElementType &returnValue);
+Status assign(BinaryTree &tree, BinaryNode node, ElementType value);
+Status searchTree(BinaryTree tree, ElementType element, BinaryTree &returnTree);
+Status getParent(BinaryTree &tree, BinaryNode node, BinaryTree &parentTree);
+Status getChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, BinaryTree &returnChild);
+Status getLeftChild(BinaryTree &tree, BinaryNode parentNode, BinaryTree &returnChild);
+Status getRightChild(BinaryTree &tree, BinaryNode parentNode, BinaryTree &returnChild);
+Status getLeftSibling(BinaryTree &tree, BinaryNode parentNode, BinaryNode &returnSibling);
+Status getRightSibling(BinaryTree &tree, BinaryNode parentNode, BinaryNode &returnSibling);
+Status insertChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, BinaryTree childTree);
+Status deleteChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, BinaryNode &returnNode);
+Status insertNode(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, ElementType value, BinaryNode &returnNode);
+Status preOrderTraverse(BinaryTree &tree);
+Status inOrderTraverse(BinaryTree &tree);
+Status postOrderTraver(BinaryTree &tree);
+Status levelOrderTraverse(BinaryTree &tree);
 /*=====================================函数声明=====================================*/
+
+
+
+/*=====================================函数定义=====================================*/
 
 /**
  * 功能：初始化二叉树
@@ -46,11 +72,25 @@ Status initBinaryTree(BinaryTree &tree){
 }
 
 /**
+ * 待测试
  * 功能：销毁树
  * 参数：tree 二叉树
  * 返回值：销毁结果
 */
-Status destoryBinaryTree(BinaryTree &tree);
+Status destoryBinaryTree(BinaryTree &tree){
+    if(isEmptyBinaryTree(tree)){
+        return TRUE;
+    }else{
+        BinaryTree leftChild = tree->leftChild;
+        BinaryTree rightChild = tree->rightChild;
+
+        destoryBinaryTree(tree);
+        destoryBinaryTree(leftChild);
+        destoryBinaryTree(rightChild);
+
+        return TRUE;
+    }
+}
 
 /**
  * 功能：创建二叉树
@@ -85,9 +125,24 @@ Status isEmptyBinaryTree(BinaryTree &tree){
 /**
  * 功能：获取二叉树的深度（层数）
  * 参数：tree 二叉树
- * 返回值：二叉树深度 小于0为错误
+ * 返回值：二叉树深度
 */
-int getBinaryTreeDepth(BinaryTree &tree);
+int getBinaryTreeDepth(BinaryTree &tree){
+    int depth = 0;
+
+    if(!isEmptyBinaryTree(tree)){
+        //如果不为空的话 那么本身算一层
+        depth++;
+        //计算左子树和右子树的深度
+        int leftTreeDepth = getBinaryTreeDepth(tree->leftChild);
+        int rightTreeDepth = getBinaryTreeDepth(tree->rightChild);
+        //树的深度是深度求子树中深度最高的 所以再加上两者中较大的值并返回
+        depth += (leftTreeDepth >= rightTreeDepth ? leftTreeDepth : rightTreeDepth);
+        return depth;
+    }else{
+        return 0;
+    }
+}
 
 /**
  * 功能：获取根结点
@@ -113,16 +168,34 @@ Status getRoot(BinaryTree &tree, ElementType &returnRoot){
  * 这个也没用
 */
 Status getValue(BinaryTree &tree, BinaryNode node, ElementType &returnValue){
-    return OK;
+    BinaryTree searchResult;
+    int resultCode = searchTree(tree, node.data, searchResult);
+
+    if(resultCode == TRUE){
+        returnValue = searchResult->data;
+        return TRUE;
+    }else{
+        return ERROR;
+    }
 }
 
 /**
  * 功能：给二叉树中某个结点赋值
  * 参数：tree 二叉树，node 某个结点，value 要赋的值
  * 返回值：赋值结果
+ * 
+ * 没用+1
 */
 Status assign(BinaryTree &tree, BinaryNode node, ElementType value){
-    return OK;
+    BinaryTree searchResult;
+    int resultCode = searchTree(tree, node.data, searchResult);
+
+    if(resultCode == TRUE){
+        searchResult->data = value;
+        return TRUE;
+    }else{
+        return ERROR;
+    }
 }
 
 /**
@@ -134,12 +207,12 @@ Status searchTree(BinaryTree tree, ElementType element, BinaryTree &returnTree){
     if(isEmptyBinaryTree(tree)){
         return FALSE;
     }else{
-        //如果当前结点元素值和要寻找的元素值相同那么直接返回
+        //如果当前结点元素值和要寻找的元素值相同那么返回该结点的指针
         if(element == tree->data){
             returnTree = tree;
             return TRUE;
         }else{
-            //否则就去搜索左子树和右子树
+            //否则就去递归搜索左子树和右子树
             int code = searchTree((BinaryTree)tree->leftChild, element, returnTree);
             if(code == TRUE)
                 return TRUE;
@@ -160,32 +233,73 @@ Status getParent(BinaryTree &tree, BinaryNode node, BinaryTree &parentTree){
 }
 
 /**
+ * 功能：获取二叉树中某个结点的子树
+ * 参数：tree 二叉树，node 结点，isLeft 是否为左子树，returnChild 通过此参数返回(左/右)子树
+ * 返回值：获取结果
+*/
+Status getChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, BinaryTree &returnChild){
+    BinaryTree searchResult;
+    int resultCode = searchTree(tree, parentNode.data, searchResult);
+
+    if(resultCode == TRUE){
+        returnChild = isLeft ? searchResult->leftChild : searchResult->rightChild;
+        return returnChild != NULL;
+    }else{
+        return ERROR;
+    }
+}
+
+/**
  * 功能：获取二叉树中某个结点的左子树
  * 参数：tree 二叉树，node 结点，returnChild 通过此参数返回左子树
  * 返回值：获取结果
 */
-Status getLeftChild(BinaryTree &tree, BinaryNode node, BinaryTree &returnChild);
+Status getLeftChild(BinaryTree &tree, BinaryNode parentNode, BinaryTree &returnChild){
+    return getChild(tree, parentNode, TRUE, returnChild);
+}
 
 /**
  * 功能：获取二叉树中某个结点的右子树
  * 参数：tree 二叉树，node 结点，returnChild 通过此参数返回右子树
  * 返回值：获取结果
 */
-Status getRightChild(BinaryTree &tree, BinaryNode node, BinaryTree &returnChild);
+Status getRightChild(BinaryTree &tree, BinaryNode parentNode, BinaryTree &returnChild){
+    return getChild(tree, parentNode, FALSE, returnChild);
+}
 
 /**
  * 功能：获取二叉树中某个结点的左兄弟
  * 参数：tree 二叉树，node 结点，returnSibling 通过此参数返回其右兄弟
  * 返回值：获取结果，如果该结点为二叉树的左子孙或没有左兄弟则返回空
 */
-Status getLeftSibling(BinaryTree &tree, BinaryTree node, BinaryTree &returnSibling);
+Status getLeftSibling(BinaryTree &tree, BinaryNode parentNode, BinaryNode &returnSibling){
+    BinaryTree returnTree;
+    Status resultCode = getChild(tree, parentNode, TRUE, returnTree);
+
+    if(resultCode == TRUE){
+        returnSibling = *returnTree;
+        return TRUE;
+    }
+
+    return FALSE;
+}
 
 /**
  * 功能：获取二叉树中某个结点的右兄弟
  * 参数：tree 二叉树，node 结点，returnSibling 通过此参数返回其右兄弟
  * 返回值：获取结果，如果该结点为二叉树的右子孙或没有右兄弟则返回空
 */
-Status getRightSibling(BinaryTree &tree, BinaryTree node, BinaryTree &returnSibling);
+Status getRightSibling(BinaryTree &tree, BinaryNode parentNode, BinaryNode &returnSibling){
+    BinaryTree returnTree;
+    Status resultCode = getChild(tree, parentNode, FALSE, returnTree);
+
+    if(resultCode == TRUE){
+        returnSibling = *returnTree;
+        return TRUE;
+    }
+
+    return FALSE;
+}
 
 /**
  * 功能：给二叉树中的某个结点插入一个（左or右）子树
@@ -197,9 +311,11 @@ Status insertChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, Bina
         tree = childTree;
         return TRUE;
     }else{
+        //查找其父节点
         BinaryTree searchResult;
         Status resultCode = searchTree(tree, parentNode.data, searchResult);
         if(resultCode == TRUE){
+            //查找成功 将要插入的子树赋值给父结点的左子树或右子树
             if(isLeft){
                 searchResult->leftChild = childTree;
             }else{
@@ -207,6 +323,7 @@ Status insertChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, Bina
             }
             return TRUE;
         }else{
+            //查找失败 返回FALSE
             return FALSE;
         }
     }
@@ -214,18 +331,24 @@ Status insertChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, Bina
 
 /**
  * 功能：删除二叉树中某个结点的左子树或右子树
- * 参数：tree 二叉树，node 结点，isLeft 左1右0
+ * 参数：tree 二叉树，parentNode 结点，isLeft 左1右0
  * 返回值：删除结果
 */
 Status deleteChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, BinaryNode &returnNode){
     if(isEmptyBinaryTree(tree)){
         return ERROR;
     }else{
+        // 查找父结点
         BinaryTree searchResult;
         int resultCode = searchTree(tree, parentNode.data, searchResult);
-        if(resultCode == TRUE){
-            returnNode = *(isLeft ? searchResult->leftChild : searchResult->rightChild);
 
+        if(resultCode == TRUE){
+            // 将父结点的左子树或者右子树赋值给参数returnTree
+            BinaryTree returnTree = (isLeft ? searchResult->leftChild : searchResult->rightChild);
+            if(returnTree != NULL)
+                returnNode = *returnTree;
+
+            // 随后删除对应子树并返回TRUE
             if(isLeft){
                 searchResult->leftChild = NULL;
             }else{
@@ -244,6 +367,7 @@ Status deleteChild(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, Bina
  * 返回值：插入结果
 */
 Status insertNode(BinaryTree &tree, BinaryNode parentNode, Boolean isLeft, ElementType value, BinaryNode &returnNode){
+    // 动态申请一个结点 并给其数据域赋值 指针域设为NULL
     BinaryTree childNode = (BinaryNode *)malloc(sizeof(BinaryNode));
     childNode->data = value;
     childNode->leftChild = childNode->rightChild = NULL;
@@ -309,4 +433,4 @@ Status postOrderTraver(BinaryTree &tree){
  * 返回值：遍历结果
 */
 Status levelOrderTraverse(BinaryTree &tree);
-/*=====================================函数声明=====================================*/
+/*=====================================函数定义=====================================*/
--- Tree/Linked/BinaryTree/test.cpp
+++ Tree/Linked/BinaryTree/test.cpp
@@ -4,7 +4,12 @@
 void printAllElement(BinaryTree &tree){
     //前序遍历
     printf("\n");
-    preOrderTraverse(tree);
+
+    if(isEmptyBinaryTree(tree))
+        printf("no element");
+    else
+        preOrderTraverse(tree);
+
     printf("\n");
 }
 
@@ -16,17 +21,26 @@ int main(){
     int initCode = initBinaryTree(binaryTree);
     printf("Init Binary Tree    Code = %d\n", initCode);
 
+    int treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
     //向二叉树中插入一个元素
     BinaryNode returnNode1;
     int insertCode1 = insertNode(binaryTree, returnNode1, TRUE, 1, returnNode1);
     printf("Insert Element%d      Code = %d\n", 1, insertCode1);
     printAllElement(binaryTree);
 
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
     //向二叉树中插入一个元素
     BinaryNode returnNode2;
     int insertCode2 = insertNode(binaryTree, *binaryTree, TRUE, 2, returnNode2);
     printf("Insert Element%d      Code = %d\n", 2, insertCode2);
     printAllElement(binaryTree);
+
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
 
 
     //向二叉树中插入一个元素
@@ -35,6 +49,9 @@ int main(){
     printf("Insert Element%d      Code = %d\n", 3, insertCode3);
     printAllElement(binaryTree);
 
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
 
     //向二叉树中插入一个元素
     BinaryNode returnNode4;
@@ -42,11 +59,17 @@ int main(){
     printf("Insert Element%d      Code = %d\n", 4, insertCode3);
     printAllElement(binaryTree);
 
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
     //向二叉树中插入一个元素
     BinaryNode returnNode5;
     int insertCode5 = insertNode(binaryTree, returnNode3, TRUE, 5, returnNode5);
     printf("Insert Element%d      Code = %d\n", 5, insertCode5);
     printAllElement(binaryTree);
+
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
 
     //向二叉树中插入一个元素
     BinaryNode returnNode6;
@@ -54,16 +77,75 @@ int main(){
     printf("Insert Element%d      Code = %d\n", 6, insertCode6);
     printAllElement(binaryTree);
 
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
     //向二叉树中插入一个元素
     BinaryNode returnNode7;
     int insertCode7 = insertNode(binaryTree, returnNode6, FALSE, 7, returnNode7);
     printf("Insert Element%d      Code = %d\n", 7, insertCode7);
     printAllElement(binaryTree);
 
-    BinaryNode returnNode8;
-    int deleteCode1 = deleteChild(binaryTree, returnNode1, TRUE, returnNode8);
-    printf("Delete Element%d      Code = %d\n", returnNode8.data, deleteCode1);
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
+    // BinaryNode returnNode8;
+    // int deleteCode1 = deleteChild(binaryTree, returnNode1, TRUE, returnNode8);
+    // printf("Delete Element%d      Code = %d\n", returnNode8.data, deleteCode1);
+    // printAllElement(binaryTree);
+
+    // BinaryNode returnNode9;
+    // deleteCode1 = deleteChild(binaryTree, returnNode2, TRUE, returnNode9);
+    // printf("Delete Element%d      Code = %d\n", returnNode9.data, deleteCode1);
+    // printAllElement(binaryTree);
+
+
+    int assignCode1 = assign(binaryTree, returnNode1, 9);
+    printf("Assign Element%d      Code = %d\n", 9, assignCode1);
     printAllElement(binaryTree);
+
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
+    int returnValue = -999;
+    returnNode1.data = 9;
+    int valueCode1 = getValue(binaryTree, returnNode1, returnValue);
+    printf("node%d value Element%d      Code = %d\n",returnNode1.data, returnValue, valueCode1);
+    printAllElement(binaryTree);
+
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
+
+    BinaryTree returnChild1;
+    returnNode1.data = 3;
+    int getChildCode1 = getChild(binaryTree, returnNode1, TRUE, returnChild1);
+    printf("Get Child, Root Element%d      Code = %d\n",getChildCode1 == TRUE ? returnChild1->data : 0, getChildCode1);
+    printAllElement(returnChild1);
+
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
+    BinaryNode returnSibling1;
+    returnSibling1.data = 999;
+    returnNode1.data = 7;
+    int getSiblingCode = getRightSibling(binaryTree, returnNode1, returnSibling1);
+    printf("Get Left Sibling Element%d      Code = %d\n", returnSibling1.data, getSiblingCode);
+    printAllElement(binaryTree);
+
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
+    //向二叉树中插入一个元素
+    BinaryNode returnNode10;
+    returnNode1.data = 9;
+    int insertCode10 = insertNode(binaryTree, returnNode1, TRUE, 8, returnNode10);
+    printf("Insert Element%d      Code = %d\n", 8, insertCode10);
+    printAllElement(binaryTree);
+
+    treeDepth = getBinaryTreeDepth(binaryTree);
+    printf("tree depth %d\n",treeDepth);
+
 
     return 1;
 }
